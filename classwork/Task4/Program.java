9337c977604c8b1b33ea63c429017d153de326d4
[LiliyaMagomedova] <[dusiasparrow@yandex.ru]>
2017-03-11 19:57:58 +0300

    add classwork task4

--- classwork/Task4/Program.java
+++ classwork/Task4/Program.java
@@ -5,14 +5,14 @@ class Program {
 		int amount [] = new int [121];
 		Random random = new Random();
 		int s = 0;
-		for(int i = 0; i < amount.length; i++) {
-			amount[i] = 0;
-		}
 
 		for(int i = 0; i < ages.length; i++) {
 			ages[i] = random.nextInt(120);
-			amount [ages[i]] +=1;
 		}
+		for(int i = 0; i < ages.length; i++) {
+			amount [ages[i]] += 1;
+		}
+
 
 		for(int i = 0; i < 100; i++) {
 			System.out.println(ages[i]);
@@ -24,7 +24,6 @@ class Program {
 			System.out.println(i +" age  - " + amount[i] + " people");
 		}
 		System.out.println(s);
-		// вывести на экран количество людей, с возрастами от 0 до 120
 		
 
 	}
